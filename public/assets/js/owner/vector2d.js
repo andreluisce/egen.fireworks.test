1b4c88846f3d69a6e3232ab86e93cbf68079be90
Andre Evangelista <andreluisce@gmail.com>
2016-12-21 06:19:38 -0200

    Last Commit

--- public/assets/js/owner/vector2d.js
+++ public/assets/js/owner/vector2d.js
@@ -1,22 +1,22 @@
-function Vector2D(x, y) {
-    this.x = x || 0;
-    this.y = y || 0;
+ function Vector2D(x, y) {
+     this.x = x || 0;
+     this.y = y || 0;
 
-    this.add = function(vector) {
-        this.x += vector.x;
-        this.y += vector.y;
-        return this;
-    }
+     this.add = function(vector) {
+         this.x += vector.x;
+         this.y += vector.y;
+         return this;
+     }
 
-    this.sub = function(vector) {
-        this.x -= vector.x;
-        this.y -= vector.y;
-        return this;
-    }
+     this.sub = function(vector) {
+         this.x -= vector.x;
+         this.y -= vector.y;
+         return this;
+     }
 
-    this.mult = function(n) {
-        this.x *= n || 0;
-        this.y *= n || 0;
-        return this;
-    };
-}
+     this.mult = function(n) {
+         this.x *= n || 0;
+         this.y *= n || 0;
+         return this;
+     };
+ }
